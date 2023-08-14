0511af337f82f1a684a14635eae4261f1df4c884
Lunarmagpie <bambolambo0@gmail.com>
2023-08-13 23:05:55 -0400

    improve service file

--- install.sh
+++ install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+set -e
 
-go build
-
-cp extra/aporia.pam /etc/pam.d/aporia
-cp extra/aporia.service /etc/systemd/system/aporia.service
-cp aporia /bin/aporia
-
-echo "It is ok this command fails"
-systemctl disable display-manager.service
-systemctl disable getty@tty1.service
-systemctl enable aporia.service
-
-mkdir /etc/aporia
+if
+  go build
+  cp extra/aporia.pam /etc/pam.d/aporia
+  cp extra/aporia.service /etc/systemd/system/aporia.service
+  cp aporia /bin/aporia
+  mkdir /etc/aporia
+then
+  set +e
+  echo -e "Run the command \033[94msystemctl enable aporia.service\033[39;49m to enable aporia"
+  echo " | See more information at https://github.com/lunarmagpie/aporia"
+else
+  set +e
+  echo -e "\033[31mAporia failed to install\033[39;49m"
+  echo " | See more information at https://github.com/lunarmagpie/aporia"
+  exit 1
+fi
